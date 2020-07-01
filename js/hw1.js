4f8ba5c42c8f630ec27c37139cca51c915878439
AnastasiiaChery <zamorena.nastya@gmail.com>
2020-07-01 13:56:56 +0300

    js

---
+++ js/hw1.js
@@ -0,0 +1,332 @@
+//HW
+
+
+//1
+
+let one = 'Hello';
+let two = 'owu';
+let three = 'com';
+let four = 'ua';
+let five = 1;
+let six = 10;
+let seven = -999;
+let eight = 123;
+let nine = 3.14;
+let ten = 2.7;
+let eleven = 16;
+let twelwe = true;
+let thirt = false;
+
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(six);
+// console.log(seven);
+// console.log(eight);
+// console.log(nine);
+// console.log(ten);
+// console.log(eleven);
+// console.log(twelwe);
+// console.log(thirt);
+
+// alert(one);
+// alert(two);
+// alert(three);
+// alert(four);
+// alert(five);
+// alert(six);
+// alert(seven);
+// alert(eight);
+// alert(nine);
+// alert(ten);
+// alert(eleven);
+// alert(twelwe);
+// alert(thirt);
+
+// document.write(one);
+// document.write(two);
+// document.write(three);
+// document.write(four);
+// document.write(five);
+// document.write(six);
+// document.write(seven);
+// document.write(eight);
+// document.write(nine);
+// document.write(ten);
+// document.write(eleven);
+// document.write(twelwe);
+// document.write(thirt);
+
+
+
+//2
+one= 'Good';
+two= 'day';
+three= 'welcome';
+four='to';
+five='page';
+six = 'it';
+seven= 'is'
+eight = true;
+nine = 'or';
+ten= false;
+eleven= 25;
+twelwe= 71;
+thirt= 3.5;
+
+
+// console.log(one);
+// console.log(two);
+// console.log(three);
+// console.log(four);
+// console.log(five);
+// console.log(six);
+// console.log(seven);
+// console.log(eight);
+// console.log(nine);
+// console.log(ten);
+// console.log(eleven);
+// console.log(twelwe);
+// console.log(thirt);
+
+// alert(one);
+// alert(two);
+// alert(three);
+// alert(four);
+// alert(five);
+// alert(six);
+// alert(seven);
+// alert(eight);
+// alert(nine);
+// alert(ten);
+// alert(eleven);
+// alert(twelwe);
+// alert(thirt);
+
+// document.write(one);
+// document.write(two);
+// document.write(three);
+// document.write(four);
+// document.write(five);
+// document.write(six);
+// document.write(seven);
+// document.write(eight);
+// document.write(nine);
+// document.write(ten);
+// document.write(eleven);
+// document.write(twelwe);
+// document.write(thirt);
+
+
+//3
+
+const first = 'One';
+const second = 'Two';
+const third = 'Three';
+const fourth = 4;
+const fifth = 5;
+const sixth = 6;
+
+// console.log(first);
+// console.log(second);
+// console.log(third);
+// console.log(fourth);
+// console.log(fifth);
+// console.log(sixth);
+
+// alert(first);
+// alert(second);
+// alert(third);
+// alert(fourth);
+// alert(fifth);
+// alert(sixth);
+
+// document.write(first);
+// document.write(second);
+// document.write(third);
+// document.write(fourth);
+// document.write(fifth);
+// document.write(sixth);
+
+
+//4
+
+// const userName = prompt('What is you name?');
+// const userSurname = prompt('What is you surname?');
+// const userNameByFather = prompt('What is you name by father?');
+
+// console.log(userName);
+// console.log(userSurname);
+// console.log(userNameByFather);
+
+// document.write(userName);
+// document.write(userSurname);
+// document.write(userNameByFather);
+
+// alert(userName);
+// alert(userSurname);
+// alert(userNameByFather);
+
+
+
+//5
+
+// const userName = prompt('What is you name?');
+// const userSurname = prompt('What is you surname?');
+// const userNameByFather = prompt('What is you name by father?');
+
+// let person = (userName+userSurname+userNameByFather);
+// alert(person);
+// console.log(person);
+// document.write(person);
+
+//6
+
+//7
+
+// const number1 = +prompt('Enter number 1');
+// const number2 = +prompt('Enter number 2');
+// const number3 = +prompt('Enter number 3');
+
+// console.log(number1 + number2+number3);
+
+//8 ?????
+
+// const number1 = prompt('Enter number 1');
+// const number2 = prompt('Enter number 2');
+// const number3 = prompt('Enter number 3');
+// const number4 = prompt('Enter number 4');
+
+// let result = (parseInt(number1)+parseInt(number2)+parseInt(number3)+parseInt(number4));
+// console.log(result);
+
+
+//9
+
+// const number1 = prompt('Enter number 1');
+// const number2 = prompt('Enter number 2');
+// const number3 = prompt('Enter number 3');
+
+
+// let result = (parseFloat(number1)+parseFloat(number2)+parseFloat(number3));
+// console.log(result);
+
+
+//10
+
+// const number1 = prompt('Enter number 1');
+// const number2 = prompt('Enter number 2');
+// const number3 = prompt('Enter number 3');
+
+
+// let result = (Math.round(number1)+Math.round(number2)+Math.round(number3));
+// console.log(result);
+
+
+//11
+
+// const number1 = prompt('Enter number 1');
+// const number2 = prompt('Enter number 2');
+
+// let result = (Math.pow(parseInt(number1),parseInt(number2)));
+// console.log(result);
+
+
+//12
+
+
+// let a=(100);
+// let b=('100');
+// let c=(true);
+// let d=(undefined);
+
+
+// console.log(typeof a);
+// console.log(a);
+
+// console.log(typeof b);
+// console.log(b);
+
+// console.log(typeof c);
+// console.log(c);
+
+// console.log(typeof d);
+// console.log(d);
+
+
+//13
+
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 === 6);
+// console.log(5 >= 6);
+// console.log(10 === 10)
+// console.log(10 >= 10)
+// console.log(10 !== 10)
+// console.log(10 > 10)
+// console.log(10 < 10)
+// console.log(123 === '123')
+// console.log(123 == '123')
+
+//14
+
+// console.log(132 > 100 && 45 < 12 ); false
+// console.log(34 > 33 && 23 < 90 ); true
+// console.log(99 > 100 && 45 > 12 ); false
+// console.log(132 > 100 || 45 < 12 ); true
+// console.log(111 > 11 || 45 < 111 ); true
+// console.log((111 > 11 || 45 < 111) && (132 > 100 || 45 < 12) ); true
+// console.log((111 > 11 || 45 < 56) || (132 > 100 || 45 < 12) ); true
+// console.log((132 > 100 && 45 < 12 ) && (132 > 100 || 45 < 12 ) ); false
+// console.log((111 > 11 || 45 < 111) || (99 > 100 && 45 > 12 )); true
+// console.log(!!'-1'); true
+// console.log(!!-1); true
+// console.log(!!'0'); true
+// console.log(!!'null'); true
+// console.log(!!'undefined'); true
+// console.log(!!(3/'owu')); false
+// console.log((111 > 11 || 45 < 111) ||  !!'0'); true
+// console.log((!!111 == !!11 || 45 < 111) && (99 > 100 && 45 > 12 )); false
+
+
+
+
+// CLASS
+
+
+//1
+
+// let str=('Привет');
+// let num=(123);
+// let flag=(true);
+// let txt=("true");
+
+
+// console.log(typeof str);
+// console.log(str);
+
+// console.log(typeof num);
+// console.log(num);
+
+// console.log(typeof flag);
+// console.log(flag);
+
+// console.log(typeof txt);
+// console.log(txt);
+
+//2
+
+let a1 =(5+3);
+let a2 =(5-3);
+let a3 =(5*3);
+let a4 =(5/3);
+let a5 =(5%3);
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);
