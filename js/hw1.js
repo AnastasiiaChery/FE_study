567529ea46fc4d96f87d17ff2c13ccb359109923
AnastasiiaChery <zamorena.nastya@gmail.com>
2020-07-02 15:15:22 +0300

    дз

--- js/hw1.js
+++ js/hw1.js
@@ -319,14 +319,190 @@ const sixth = 6;
 
 //2
 
-let a1 =(5+3);
-let a2 =(5-3);
-let a3 =(5*3);
-let a4 =(5/3);
-let a5 =(5%3);
+// let a1 =(5+3);
+// let a2 =(5-3);
+// let a3 =(5*3);
+// let a4 =(5/3);
+// let a5 =(5%3);
 
-console.log(a1);
-console.log(a2);
-console.log(a3);
-console.log(a4);
-console.log(a5);
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+//3
+
+
+// let a6 =(5%3);
+// let a7 =(3%5);
+// let a8 =(5+'3');
+// let a9 =('5'-3);
+// let a10 =(75+'kg');
+
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+
+
+//4
+
+// let height = (23);
+// let width = (10);
+// let s = (height*width);
+// console.log(height);
+// console.log(width);
+// console.log(s);
+
+
+
+//5
+
+// let heightC = (10);
+// let dC = (4);
+// let v = (heightC*dC);
+
+// console.log(heightC);
+// console.log(dC);
+// console.log(v);
+
+
+//6
+
+// let a = (3);
+// let b = (4);
+// let c = (Math.pow((Math.pow(a,2)+Math.pow(b,2)),0.5))
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//7
+
+// let str =('Hello world');
+// alert(str);
+// console.log(str);
+// document.write(str);
+
+
+//8
+
+// let fio = ('Chereshnia Anastasiia Leonidivna');
+// let age = (23);
+// let hobby = ('cooking');
+// alert(fio + '\n' + age + '\n' + hobby);
+
+
+//9
+
+// let str1 =('Кто');
+// let str2 =('ты');
+// let str3 =('такой?');
+// let concatenation =( str1+str2+str3);
+// document.write(concatenation)
+
+//10
+
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");  205
+// document.write(str - a + "<br/>");  15
+// document.write(str * "2" + "<br/>"); 40
+// document.write(str / 2 + "<br/>"); 10
+
+
+
+//11
+
+
+// parseInt("3.14")  3
+// parseInt("-7.875") -7
+// parseInt("435") 435
+// parseInt("Вася") Nan
+
+//12
+
+// let str = prompt("Enter something", "ho-ho")
+// console.log(str)
+
+
+//13
+
+// const number1 = +prompt('Enter number 1');
+// const number2 = +prompt('Enter number 2');
+
+// alert(number1 + number2);
+
+
+//14
+
+// const name = prompt('Enter name');
+// const surname = prompt('Enter surname');
+// const age = prompt('Enter age');
+
+// alert("Доброго вечера" + ' ' + name + ' ' + surname + ' ' + "мои поздравления что вам" + ' ' +  age)
+
+
+
+//ДОП
+
+
+//1
+// let number1 = +prompt('Enter number 1');
+// let number2 = +prompt('Enter number 2');
+// let number3 = +prompt('Enter number 3');
+
+
+// if ( number1 > number2) { let tmp_number = number1; number1 = number2; number2 = tmp_number; };
+// if ( number2 > number3)  { let tmp_number = number2; number2 = number3; number3 = tmp_number; };
+// if ( number1 > number2) { let tmp_number = number1; number1 = number2; number2 = tmp_number; };
+
+//       alert(number1);
+//       alert(number2);
+//       alert(number3);
+
+
+
+//2
+
+// let color = prompt('Введите цвет');
+
+// if (color === "зеленый") {
+//       alert('Иди')
+//     } else if (color === "желтый") {
+//       alert('подожди')
+//     } else if (color === "красный") {
+//         alert('Жди')
+//       } 
+//     else {
+//       alert('делай что хочешь')
+//     }
+
+
+//3
+
+
+// let color = prompt('Введите цвет');
+// let car = confirm('Есть ли машины?');
+
+// if (color === "зеленый" && !car) {
+//           alert('Иди')
+//         } else if (color === "зеленый" && car) {
+//           alert('подожди пока нарушители проедут')
+//         } else if (color === "желтый" && car) {
+//             alert('Жди')
+//           } 
+//           else if (color === "желтый" && !car) {
+//             alert('Все равно жди')
+//           } 
+//           else if (color === "красный" && !car) {
+//             alert('Стой все равно')
+//           } 
+//           else if (color === "красный" && car) {
+//             alert('Стой и жди')
+//           } 
+//         else {
+//           alert('делай что хочешь')
+//         }
