0a682c8085484042c1fa623d184dcc6ffa8a6f11
AnastasiiaChery <zamorena.nastya@gmail.com>
2020-07-04 13:00:17 +0300

    hw2

---
+++ js hw2/hw2.js
@@ -0,0 +1,79 @@
+//1
+  
+// let numbers = [52, 5, 7, 12,83]
+// console.log(numbers);
+
+//2
+
+// let names = ['Katya', 'Olya', 'Igor', 'Maria', 'Oleg']
+// console.log(names);
+
+//3
+// let all = ['olya', 6, true, true, 9]
+// console.log(all);
+
+//4
+// let empty = [];
+// empty[0]= 'Olya';
+// empty[1] = 7;
+// empty[2] = true;
+// empty[3]= 12;
+// empty[4]= 'Katya';
+// console.log(empty)
+
+
+//5
+// for (let i = 0; i < 10; i++) {
+// document.write('<div> text</div>');
+// }
+
+//6
+
+// for (let i = 0; i < 10; i++) {
+// document.write('<div> text'+i+'</div>');
+// }
+
+// 7
+
+// let i = 1;
+// while (i< 21) {
+// document.write('<h1> text</h1>');
+// i+=1;
+// }
+
+//8
+
+// let i = 1;
+// while (i< 21) {
+// document.write('<h1> text'+i+'</h1>');
+// i+=1;
+// }
+
+
+
+//9
+
+// let numbers = [ 2, 12, 6, 93, 73, 1, 93, 3, 90, 4 ];
+// for ( let i =0; i< numbers.length; i++){
+// console.log(numbers [i])
+// }
+
+
+// //10
+
+// let names = ['Anya', 'Katya', 'Oleg', 'Igor', 'Sveta', 'Inna', 'Dima', 'Sasha', 'Alina', 'Vasya'];
+// for (let i = 0; i < names.length; i++) {
+// console.log(names[i]);
+// }
+
+
+// 11
+
+// let all = [12, 'Katya', true, 93, 'Sveta', false, 'Dima', 94, 'Alina', 'Vasya'];
+// for (let i = 0; i < all.length; i++) {
+// console.log(all[i]);
+// }
+
+//12
+
+let all = [12, 'Katya', true, 93, 'Sveta', false, 'Dima', 94, 'Alina', 'Vasya'];
